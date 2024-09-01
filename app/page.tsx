6dfbaeaab6e28c90bea12f6f9d3ec66117fe39ec
Vlad Dumitru <vlad.dumitru@tokero.dev>
2024-09-01 11:12:55 +0300

    Install the Prisma ORM

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+import prisma from "@/prisma/db";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const prismaTest = await prisma.listing.findMany();
+
   return (
     <div>
-      Test
+      {prismaTest.map((listing) => {
+        return (
+          <div key={listing.id}>
+            <Image
+              src={listing.image}
+              alt={listing.title}
+              width={320}
+              height={180}
+            />
+            <h2>{listing.title}</h2>
+            <p>{listing.description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  trailingSlash: true,
+};
 
 export default nextConfig;
